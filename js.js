028da6b2c323d74333bd4a9f718fbcf6641a1f98
docwali777 <drgauvin@mgmcare.org>
2017-01-29 00:57:30 -0500

    To finish - need to redo map - zoom  and resize meteor circles

--- js.js
+++ js.js
@@ -1,33 +1,78 @@
 
 let url = "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/meteorite-strike-data.json",
     worldfile = "https://rawgit.com/Docwali777/mapdatavisualization/master/world-map.json",
-    h = window.innerHeight,
-    w = window.innerWidth;
+    h = 1000,
+    w = 1400;
 let projection =
 
 d3.geoMercator()
- // .center([-81.369515, 28.538479])
- .scale(130)
-// 	.translate([w, h / 2]);
+ //.center([-81.369515, 28.538479])
+ .scale(100)
+ 	.translate([w / 2, h / 2]);
 
 
 
 let path = d3.geoPath()
   .projection(projection)
 
-  var svg = d3.select("body").append("svg").attr("height", h).attr("width", w)
-  //.style("border", "1px solid black")
+  var zoom = d3.zoom()
+      .on("zoom", zoomed);
+
+      function zoomed() {
+    view.attr("transform", "translate(" + d3.event.translate + ") scale(" + d3.event.scale + ")");
+  }
+
+  function clicked(d, i) {
+    if (d3.event.defaultPrevented) return; // zoomed
+
+    d3.select(this).transition()
+        .style("fill", "black")
+      .transition()
+        .style("fill", "white");
+  }
+
+  function nozoom() {
+    d3.event.preventDefault();
+  }
+
+
+  var svg = d3.select("body")
+  .on("touchstart", nozoom)
+      .on("touchmove", nozoom)
+  .append("svg").attr("height", h).attr("width", w)
+  var g = svg.append("g")
+      .call(zoom);
 
 d3.queue()
     .defer(d3.json, url)
    .await(meteorites)
 
+
+
+
+   var tip = d3.tip()
+
+
 //Access JSON data for Meteorite-strike-data
 function meteorites()
 {d3.json(url, function(data){
 
+  tip.attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function(d) {
+  return `Name: ${d.properties.name} <br>
+    Mass: ${d.properties.mass} <br>
+      Reclong: ${d.properties.reclong} <br>
+        Reclate: ${d.properties.reclat} <br>`;
+  });
+    // tip.text(function(d) {
+    //   console.log(d.features.properties.mass)
+    // })
+
 
 var circle = svg.selectAll("class", "  circle").data(data.features).enter().append("circle")
+
+
 
 circle.attr("class", "circle")
 .attr("cx", function(d){
@@ -40,8 +85,16 @@ if (d.geometry ===null){return 0}
 else {return projection(d.geometry.coordinates)[1]}
 
 })
-// console.log(data)
+.on('mouseover', tip.show)
+.on('mouseout', tip.hide);
+
+svg.call(tip)
+
+console.log(data.features[0].properties)
 })
+
+
+
 
 }
 
@@ -54,5 +107,5 @@ svg.selectAll("path")
 
 
 
-console.log(map.features[0].properties.sovereignt)
+
 })
